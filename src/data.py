b594744a2a3359252d2c18128658191c9d96bd85
Said Osmanov <saidelleausmaneuff@gmail.com>
2025-09-18 19:07:29 +0300

    Change load_tokenized_data to load_data

--- src/data.py
+++ src/data.py
@@ -1,12 +1,28 @@
-from transformers import AutoTokenizer, PreTrainedTokenizerBase
-from datasets import load_dataset
+from transformers import AutoTokenizer, PreTrainedTokenizerBase, DataCollatorWithPadding
+from datasets import load_dataset, Dataset, DatasetDict
+from torch.utils.data import DataLoader, Dataset as TorchDataset
 from typing import Tuple
 from src.types import DatasetType 
 
 
+class HFDatasetWrapper(TorchDataset):
+    def __init__(self, hf_dataset):
+        self.dataset = hf_dataset
+
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, index):
+        return self.dataset[index]
+
 def load_tokenized_data(dataset_name: str, 
-                        model_name: str
-                        ) -> Tuple[DatasetType, PreTrainedTokenizerBase]:
+                        model_name: str,
+                        batch_size: int,
+                        num_workers: int
+                        ) -> Tuple[DataLoader, 
+                                   DataLoader,
+                                   DataLoader,
+                                   PreTrainedTokenizerBase]:
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     dataset = load_dataset(dataset_name)
     
@@ -14,4 +30,32 @@ def load_tokenized_data(dataset_name: str,
         return tokenizer(batch["text"], truncation=True)
 
     tokenized_dataset = dataset.map(tokenize, batched=True)
-    return tokenized_dataset, tokenizer
+    tokenized_dataset = tokenized_dataset.rename_column("label", "labels")
+    if isinstance(tokenized_dataset, (Dataset, DatasetDict)):
+        tokenized_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
+    else:
+        raise ValueError("The dataset_name must refers to dataset with type Dataset | DatasetDict.\n"
+                         "Check if your dataset is of type IterableDataset | IterableDatasetDict, "
+                         "which are currently not supported")
+    
+    train_dataset = HFDatasetWrapper(tokenized_dataset["train"])
+    val_dataset = HFDatasetWrapper(tokenized_dataset["validation"])
+    test_dataset = HFDatasetWrapper(tokenized_dataset["test"])
+
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors="pt")
+
+    train_loader = DataLoader(dataset=train_dataset,
+                              batch_size=batch_size,
+                              shuffle=True,
+                              num_workers=num_workers,
+                              collate_fn=data_collator)
+    val_loader = DataLoader(dataset=val_dataset,
+                              batch_size=batch_size,
+                              num_workers=num_workers,
+                              collate_fn=data_collator)
+    test_loader = DataLoader(dataset=test_dataset,
+                              batch_size=batch_size,
+                              num_workers=num_workers,
+                              collate_fn=data_collator)
+    
+    return train_loader, val_loader, test_loader, tokenizer
