d83ee04664de0368ad2aec26f168eec5f8920f52
Said Osmanov <saidelleausmaneuff@gmail.com>
2025-11-19 12:58:58 +0300

    Remove unnecessary wrapping into torch.utils.data.Dataset

--- src/data.py
+++ src/data.py
@@ -6,21 +6,11 @@ import torch
 import transformers
 
 
-class HFDatasetWrapper(torch.utils.data.Dataset):
-    def __init__(self, hf_dataset):
-        self.dataset = hf_dataset
-
-    def __len__(self):
-        return len(self.dataset)
-    
-    def __getitem__(self, index):
-        return self.dataset[index]
-
 def load_data(
-        dataset_name: str, 
-        model_name: str,
-        batch_size: int,
-        num_workers: int
+    dataset_name: str, 
+    model_name: str,
+    batch_size: int,
+    num_workers: int
 ) -> Tuple[
     torch.utils.data.DataLoader, 
     torch.utils.data.DataLoader,
@@ -41,25 +31,26 @@ def load_data(
         raise ValueError("The dataset_name must refers to dataset with type Dataset | DatasetDict.\n"
                          "Check if your dataset is of type IterableDataset | IterableDatasetDict, "
                          "which are currently not supported")
-    
-    train_dataset = HFDatasetWrapper(tokenized_dataset["train"])
-    val_dataset = HFDatasetWrapper(tokenized_dataset["validation"])
-    test_dataset = HFDatasetWrapper(tokenized_dataset["test"])
 
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors="pt")
 
-    train_loader = DataLoader(dataset=train_dataset,
-                              batch_size=batch_size,
-                              shuffle=True,
-                              num_workers=num_workers,
-                              collate_fn=data_collator)
-    val_loader = DataLoader(dataset=val_dataset,
-                              batch_size=batch_size,
-                              num_workers=num_workers,
-                              collate_fn=data_collator)
-    test_loader = DataLoader(dataset=test_dataset,
-                              batch_size=batch_size,
-                              num_workers=num_workers,
-                              collate_fn=data_collator)
-    
+    train_loader = DataLoader(
+        dataset=tokenized_dataset["train"], # type: ignore
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        collate_fn=data_collator
+    )
+    val_loader = DataLoader(
+        dataset=tokenized_dataset["validation"], # type: ignore
+        batch_size=batch_size,
+        num_workers=num_workers,
+        collate_fn=data_collator
+    )
+    test_loader = DataLoader(
+        dataset=tokenized_dataset["test"], # type: ignore
+        batch_size=batch_size,
+        num_workers=num_workers,
+        collate_fn=data_collator
+    )
     return train_loader, val_loader, test_loader, tokenizer
