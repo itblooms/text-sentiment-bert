a6a537f8add33212689be5b496d0ede737a50b2e
Said Osmanov <saidelleausmaneuff@gmail.com>
2025-11-19 20:58:36 +0300

    Add config parser and fine-tuning method router

--- src/custom_types.py
+++
@@ -1,4 +0,0 @@
-from datasets import Dataset, DatasetDict, IterableDataset, IterableDatasetDict
-from typing import TypeAlias, Union
-
-DatasetType: TypeAlias = Union[Dataset, DatasetDict, IterableDataset, IterableDatasetDict]
