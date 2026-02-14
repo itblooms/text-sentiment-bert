21dafb9c4cfda18f7cc3185ae2fcffb4df102818
Said Osmanov <saidelleausmaneuff@gmail.com>
2026-02-14 23:47:53 +0300

    Delete evaluate file as it's not used anywhere

--- src/evaluate.py
+++
@@ -1,9 +0,0 @@
-import evaluate
-import torch
-
-
-def compute_metrics(eval_pred):
-    accuracy = evaluate.load("accuracy")
-    predictions, labels = eval_pred
-    predictions = torch.argmax(predictions, dim=1)
-    return accuracy.compute(predictions=predictions, references=labels)
